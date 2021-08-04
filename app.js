d8d0777625f8eac41c602164d1c9325ce59fca5f
Julie Nisbet <jmnisbet@gmail.com>
2021-08-04 15:44:14 -0700

    FEEDBACK  - DO NOT MERGE

--- app.js
+++ app.js
@@ -6,18 +6,6 @@ import { createUser } from './user/create-user.js';
 
 const userForm = document.getElementById('user-form');
 const logInBtn = document.getElementById('login-btn');
-// const 
-// const createBtn = document.getElementById('create-btn');
-
-// set event listeners 
-
-// userForm.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const formData = new FormData(userForm);
-//     const user = getUser(formData);
-//     // add function find by item to look for existing users
-//     // window.location.replace = './list';
-// });
 
 userForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -35,16 +23,7 @@ logInBtn.addEventListener('click', (e) => {
     if (oldUser.password === formData.get('password')) {
         window.location.replace('./choice-page');
         return alert('Welcome Back');
-    } else if (oldUser.password === !oldUser.password){
-        // window.location.reload();
     } else {
         return alert('Wrong Password');
     }
 });
-// reference needed DOM elements
-
-
-// set event listeners 
-  // get user input(s)
-  // do any needed work with the value(s)
-  // update DOM to reflect new value(s)
